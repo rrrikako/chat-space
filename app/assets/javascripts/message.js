31f731fced96f293aa8ba7e91abb3c69ee2075fb
Rikako Kamata <rrrikako0522@gmail.com>
2019-10-10 19:15:53 +0900

    edit for ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function(){
+  function buildMessage(message){
+    var image = message.image ? `<img class="lower-message__image" src=${message.image}>` : "";
+    var html = `<div class="message">
+                  <div class="message__user__name">
+                    ${message.user_name}
+                  </div>
+                  <div class="message__created__at">
+                    ${message.created_at}
+                  </div>
+                    <div class="message__content">
+                      <p class="lower__message__content">
+                        ${message.content}
+                      </p>
+                        ${image}
+                    </div>
+                </div>`
+    return html;
+   }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +29,13 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildMessage(data);
+      $('.contents__messages').append(html);
+    })
+    .fail(function(data){
+      alert('error');
     })
   })
 })
