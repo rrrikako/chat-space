cbfa8bb990ed050a286b3d1efe30046ba96af1c3
Rikako Kamata <rrrikako0522@gmail.com>
2019-10-12 20:05:48 +0900

    edit for improvement

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,15 +4,14 @@ $(function(){
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
     search_list.append(html);
   }
   function appendErrMsg(msg){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${msg}</p>
-                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-                  </div>`
+                </div>`
     search_list.append(html);
   }
   function appendMembers(name, user_id){
@@ -45,7 +44,7 @@ $(function(){
       }
       else{
         $('#user-search-result').empty();   
-        appendErrMsg('一致するメッセージが見つかりません');
+        appendErrMsg('一致するユーザが見つかりません');
       }
     })
     .fail(function(){
